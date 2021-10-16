0a88656b77ea847a21c8b8cdccbd67b19b2049f1
mahadevann@gmail.com <mahadevann@gmail.com>
2021-10-16 12:05:29 -0400

    ne page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomepageComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
