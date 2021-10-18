e0e9c099ffce8451f2a271501eefdb9f86289a2c
mahadevann@gmail.com <mahadevann@gmail.com>
2021-10-18 19:18:19 -0400

    new page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-homepage></app-homepage>',
+  template: '<app-homepage2></app-homepage2>',
 })
 export class AppComponent {
   value = 'World';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { Homepage2Component } from './homepage2/homepage2.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, Homepage2Component],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
